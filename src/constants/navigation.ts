96374f2e3450d740e25fe91adabf19c27756dea0
Jasper Jamir <jasperjamir@gmail.com>
2026-02-08 23:28:51 +0800

    feat(layout): nagvar

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,12 +1,54 @@
 export const LOGO_URL = "https://static.wixstatic.com/media/d09b7d_a74d0a2393f94bc8b622fc84e85214b5~mv2.jpg/v1/fill/w_200,h_176,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/Creekside_logo_2x2_120dpi_2019.jpg";
 
-export const NAV_LINKS = [
+export interface NavLinkChild {
+  name: string;
+  href: string;
+}
+
+export interface NavLink {
+  name: string;
+  id: string;
+  href: string;
+  children?: readonly NavLinkChild[];
+}
+
+export const NAV_LINKS: readonly NavLink[] = [
   { name: 'Home', id: 'home', href: '/' },
-  { name: 'Who We Are', id: 'who-we-are', href: '/who-we-are/' },
-  { name: 'What We Do', id: 'what-we-do', href: '/what-we-do/' },
+  {
+    name: 'Who We Are',
+    id: 'who-we-are',
+    href: '/who-we-are/',
+    children: [
+      { name: 'Our Story', href: '/who-we-are/our-story/' },
+      { name: 'Our Team', href: '/who-we-are/our-team/' },
+      { name: 'What is Profound Autism?', href: '/who-we-are/what-is-profound-autism/' },
+    ],
+  },
+  {
+    name: 'What We Do',
+    id: 'what-we-do',
+    href: '/what-we-do/',
+    children: [
+      { name: 'Creekside Adult Program', href: '/what-we-do/adult-program/' },
+      { name: 'Community-Based Instruction (CBI)', href: '/what-we-do/community-partnerships/' },
+    ],
+  },
   { name: 'Events', id: 'events', href: '/events/' },
-  { name: 'Families', id: 'families', href: '/families/' },
-  { name: 'Support', id: 'support', href: '/support/' },
+  {
+    name: 'Families',
+    id: 'families',
+    href: '/families/',
+    children: [
+      { name: 'Parent Resources', href: '/families/parent-resources/' },
+      { name: 'Admissions', href: '/families/admissions-process/' },
+    ],
+  },
+  {
+    name: 'Support',
+    id: 'support',
+    href: '/support/',
+    children: [{ name: 'Donate', href: '/support/donate/' }],
+  },
   { name: 'Join Us', id: 'join', href: '/join-us/' },
 ];
 
