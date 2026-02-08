65c71d9a3fdee3822de4a2593b534341deb89dd5
Jasper Jamir <jasperjamir@gmail.com>
2026-02-08 20:09:22 +0800

    ref(sections)

--- astro/src/constants/index.ts
+++ astro/src/constants/index.ts
@@ -1,4 +1,5 @@
 export { COLORS } from './colors';
+export { HERO } from './hero';
 export { LOGO_URL, NAV_LINKS, QUICK_LINKS } from './navigation';
 export { PILLARS } from './pillars';
 export { TESTIMONIALS } from './testimonials';
