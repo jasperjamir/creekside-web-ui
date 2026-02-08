4738a5169bd0074fbf387a58b396134eed949532
Jasper Jamir <jasperjamir@gmail.com>
2026-02-08 20:57:31 +0800

    feat(no pic)

---
+++ src/constants/assets.ts
@@ -0,0 +1,2 @@
+/** Base URL for external assets (images). Change when CDN or host changes. */
+export const ASSETS_BASE_URL = 'https://assets.creekside.banyuhay.ph';
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
+export { ASSETS_BASE_URL } from './assets';
 export { COLORS } from './colors';
 export { HERO } from './hero';
 export { LOGO_URL, NAV_LINKS, QUICK_LINKS } from './navigation';
