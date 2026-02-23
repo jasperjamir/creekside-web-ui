e159f480b827463454116cbcc98cb212b9ba38fb
Angel Zoleta <angel.banyuhay@gmail.com>
2026-02-23 10:11:38 +0800

    reword CBI and change order

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -29,8 +29,8 @@ export const NAV_LINKS: readonly NavLink[] = [
     id: 'what-we-do',
     href: '/what-we-do/',
     children: [
+      { name: 'The Creekside School', href: '/what-we-do/community-based-instruction/' },
       { name: 'Creekside Adult Program', href: '/what-we-do/#adult-program' },
-      { name: 'Community-Based Instruction', href: '/what-we-do/community-based-instruction/' },
     ],
   },
   {
